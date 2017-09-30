c42231e43c93800b851faf92600e2152e060624c
Pierre-François de Soos <pfdesoos@simplon.co>
2017-09-30 20:31:38 +0200

    agencement controles

--- evenements.php
+++ evenements.php
@@ -5,50 +5,54 @@ $navbar = '<li><a href="index.php">Accueil</a></li>
             <li  class="active"><a href="#">Evevenements</a></li>';
 
 $body = '
-<div class="form-group">
-    <label for="exampleSelect2" class="col-md-2">Thématique</label>
-    <div class="col-md-4">
-      <select multiple class="form-control"  id="exampleSelect2">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-      </select>
+<div class="row" style="margin-top:20px;">
+  <div class="form-group col-md-4">
+      <label for="exampleSelect2" class="col-md-4">Thématique</label>
+      <div class="col-md-8">
+        <select multiple class="form-control"  id="exampleSelect2">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
     </div>
-  </div>
 
-  <div class="form-group">
-    <label for="exampleSelect2" class="col-md-2">Typologie</label>
-    <div class="col-md-4">
-      <select multiple class="form-control" id="exampleSelect2">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-      </select>
+    <div class="form-group col-md-4">
+      <label for="exampleSelect2" class="col-md-4">Typologie</label>
+      <div class="col-md-8">
+        <select multiple class="form-control" id="exampleSelect2">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
     </div>
-  </div>
 
-<div class="form-group">
-  <label for="example-date-input" class="col-md-2 col-form-label">Date début</label>
-  <div class="col-md-4">
-    <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
-  </div>
-</div>
+  <div class="form-group col-md-4">
+    <div class="row">
+      <label for="example-date-input" class="col-md-5 col-form-label">Date début</label>
+      <div class="col-md-7">
+        <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
+      </div>
+    </div>
 
-<div class="form-group">
-  <label for="example-date-input" class="col-md-2 col-form-label">Date fin</label>
-  <div class="col-md-4">
-    <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
-  </div>
-</div>
+    <div class="row">
+      <label for="example-date-input" class="col-md-5 col-form-label">Date fin</label>
+      <div class="col-md-7">
+        <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
+      </div>
+    </div>
 
-<div class="form-group">
-  <label for="example-date-input" class="col-md-2 col-form-label">Distance (km)</label>
-  <div class="col-md-4">
-    <input class="form-control" type="number" id="example-number-input">
+    <div class="row">
+      <label for="example-date-input" class="col-md-5 col-form-label">Distance (km)</label>
+      <div class="col-md-7">
+        <input class="form-control" type="number" value=30 id="example-number-input">
+      </div>
+    </div>
   </div>
 </div>
 
